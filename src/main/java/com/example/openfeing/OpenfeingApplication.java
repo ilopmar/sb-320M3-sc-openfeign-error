1d2a153f0124a732fa836dfbf21f3e5206502fd4
Iván López <lopez.ivan@gmail.com>
2023-09-22 15:19:52 +0200

    Initial commit

---
+++ src/main/java/com/example/openfeing/OpenfeingApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfeingApplication.class, args);
+	}
+
+}
